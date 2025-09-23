e0c4c1be3a5df4cc1555cac5d066fddc9ed49854
IMANARIYO <imanariyobaptiste@gmail.com>
2025-09-23 12:40:06 +0200

    addingthe  true  url

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,7 +2,7 @@ import axios, { type AxiosRequestConfig } from "axios";
 import { tokenStorage } from "./tokenStorage";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URLvb,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
     "Cache-Control": "no-cache",
